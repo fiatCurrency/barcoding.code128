3471aaa8310bd0a7bd6ae9ade6bdc0affc83c433
fiatCurrency <fiatCurrency@github.com>
2022-11-04 00:48:53 +0000

    Add toString() and createFrom() members to CodeSet enumeration. Not sure if this belongs here.

--- code128/code128/code128support/src/main/java/uk/co/quantril/barcoding/code128/code128support/CodeSet.kt
+++ code128/code128/code128support/src/main/java/uk/co/quantril/barcoding/code128/code128support/CodeSet.kt
@@ -3,5 +3,25 @@ package uk.co.quantril.barcoding.code128.code128support
 public enum class CodeSet {
     Seta,
     Setb,
-    Setc
+    Setc;
+
+    companion object {
+        fun createFrom(c: Char): CodeSet {
+            return when(c) {
+                'A' -> Seta
+                'B' -> Setb
+                'C' -> Setc
+                else -> throw IllegalArgumentException()
+            }
+        }
+    }
+
+    public override fun toString(): String {
+        return when(this) {
+            Seta -> "A"
+            Setb -> "B"
+            Setc -> "C"
+        }
+    }
+
 }
